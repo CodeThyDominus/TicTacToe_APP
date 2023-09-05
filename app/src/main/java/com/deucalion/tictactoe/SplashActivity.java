8c3ed247779b4cd7d176ba97c999afc8d55cc3a2
CodeThyDominus <thydominus@gmail.com>
2023-09-06 05:27:42 +0530

    adding lottie animation

--- app/src/main/java/com/deucalion/tictactoe/SplashActivity.java
+++ app/src/main/java/com/deucalion/tictactoe/SplashActivity.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class SplashActivity extends AppCompatActivity {
+
+    LottieAnimationView lView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        lView = findViewById(R.id.animation_lottie);
+
+        lView.setAnimation(R.raw.animationtictactoe);
+        lView.playAnimation();
 
         Handler handler = new Handler();
 
@@ -19,13 +28,16 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
 
+                lView.cancelAnimation();
+
                 Intent iMain = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(iMain);
 
                 finish();
 
             }
-        },1500);
+        },4400);
+
 
 
     }
